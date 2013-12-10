4ca0870595ae3b512cacbd4687998e54a2173271
Andrew Stein <ahstein@gmail.com>
2013-12-10 12:21:16 -0800

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,18 +2,25 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+ let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+ 
+ describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-    
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
-  end
+   it "should have the content 'Sample App'" do
+     visit '/static_pages/home'
+     expect(page).to have_content('Sample App')
+   end
+
+   it "should have the base title" do
+     visit '/static_pages/home'
+     expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+   end
+
+   it "should not have a custom page title" do
+     visit '/static_pages/home'
+     expect(page).not_to have_title('| Home')
+   end
+ end
 
   describe "Help page" do
 
@@ -23,7 +30,7 @@ describe "Static pages" do
     end
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -35,7 +42,21 @@ describe "Static pages" do
     end
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+      expect(page).to have_title("#{base_title}  | About")
     end
   end
+  
+  describe "Contact page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Sample App')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title}  | Contact")
+    end
+  end
+  
 end
